08b4324ea50ba51aa01ebc43e4271d28fd0a9478
Underc0de <underc0de.org@gmail.com>
2020-11-05 19:38:43 -0300

    page update

--- src/test/java/starter/pages/ForoPage.java
+++ src/test/java/starter/pages/ForoPage.java
@@ -1,10 +1,11 @@
 package starter.pages;
 
 import net.serenitybdd.core.annotations.findby.FindBy;
+import net.thucydides.core.pages.PageObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-public class ForoPage {
+public class ForoPage extends PageObject {
     @FindBy(xpath = "/html/body/div[2]/div[1]/div[2]/table/tbody[8]/tr[1]/td[2]/a")
     private WebElement underc0deSection;
 

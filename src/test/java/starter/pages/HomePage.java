e349f7191a6ecd4022bd4d02189f3aeabe30a090
Underc0de <underc0de.org@gmail.com>
2020-11-05 19:40:57 -0300

    page object update

--- src/test/java/starter/pages/HomePage.java
+++ src/test/java/starter/pages/HomePage.java
@@ -1,10 +1,11 @@
 package starter.pages;
 
 import net.serenitybdd.core.annotations.findby.FindBy;
+import net.thucydides.core.pages.PageObject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-public class HomePage {
+public class HomePage extends PageObject {
 
     @FindBy(className = "btn")
     private WebElement foroButton;

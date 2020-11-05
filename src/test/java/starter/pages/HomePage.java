5b15d7ae6d258090d0f0c8cbd901b9d0b3f9f076
juanpa-pisano <jpisano@serv.externos.prismamp.com>
2020-11-05 19:14:47 -0300

    importar clase StepScenarios

--- src/test/java/starter/pages/HomePage.java
+++ src/test/java/starter/pages/HomePage.java
@@ -3,14 +3,17 @@ package starter.pages;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import net.thucydides.core.pages.PageObject;
 
-public class HomePage {
+
+
+public class HomePage extends PageObject {
 
     @FindBy(className = "btn")
     private WebElement foroButton;
 
     public void clickForoButton(){
-        foroButton.click();
+       foroButton.click();
     }
 
 }
--- src/test/java/starter/steps/homeSteps.java
+++ src/test/java/starter/steps/homeSteps.java
@@ -4,13 +4,14 @@ package starter.steps;
 import io.cucumber.java.en.When;
 import net.thucydides.core.annotations.Steps;
 import starter.pages.ForoPage;
+import net.thucydides.core.steps.ScenarioSteps;
 
-public class homeSteps {
+public class homeSteps extends ScenarioSteps {
 
     @Steps
     ForoPage foroPage;
 
-    @When("Verifico que exista la seccion \"(.*)\"")
+    @When("^Verifico que exista la seccion '(.*)'$")
     public void foroHome(String term) {
         foroPage.underc0deSection();
     }
